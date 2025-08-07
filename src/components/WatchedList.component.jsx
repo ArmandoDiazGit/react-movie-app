903fb242344cf316292cbb8fc131d55b839dde49
ArmandoDiazGit <78942285+ArmandoDiazGit@users.noreply.github.com>
2025-08-07 10:55:14 -0400

    safety operators

--- src/components/WatchedList.component.jsx
+++ src/components/WatchedList.component.jsx
@@ -3,7 +3,7 @@ import WatchedListItem from "./WatchedListItem.component";
 export default function WatchedList({ watched, handleDeleteWatched }) {
   return (
     <ul className="list">
-      {watched.map((movie) => (
+      {watched?.map((movie) => (
         <WatchedListItem
           key={movie.imdbID}
           movie={movie}
--- src/components/WatchedListItem.component.jsx
+++ src/components/WatchedListItem.component.jsx
@@ -1,24 +1,24 @@
 export default function WatchedListItem({ movie, handleDeleteWatched }) {
   return (
     <li>
-      <img src={movie.poster} alt={`${movie.Title} poster`} />
+      <img src={movie?.poster} alt={`${movie?.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
         <p>
           <span>⭐️</span>
-          <span>{movie.imdbRating}</span>
+          <span>{movie?.imdbRating}</span>
         </p>
         <p>
           <span>🌟</span>
-          <span>{movie.userRating}</span>
+          <span>{movie?.userRating}</span>
         </p>
         <p>
           <span>⏳</span>
-          <span>{movie.runtime} min</span>
+          <span>{movie?.runtime} min</span>
         </p>
         <button
           className="btn-delete"
-          onClick={() => handleDeleteWatched(movie.imdbID)}
+          onClick={() => handleDeleteWatched(movie?.imdbID)}
         >
           X
         </button>
